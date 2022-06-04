a7e5d1b43be078ce32db0df80fbe75bd0cf532d6
Noah Miyabayashi <ac3po6@gmail.com>
2022-06-04 21:42:36 +0800

    Initial Commit

---
+++ apparelfit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apparelfit
+//
+//  Created by user220178 on 6/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ apparelfitUITests/apparelfitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  apparelfitUITestsLaunchTests.swift
+//  apparelfitUITests
+//
+//  Created by user220178 on 6/4/22.
+//
+
+import XCTest
+
+class apparelfitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
